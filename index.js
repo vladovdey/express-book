e5ef7a23d5b17526010c272c49d36dcc9b074e5c
Vladislav Ovdeychuk <ovdeyvlad@gmail.com>
2022-05-07 19:23:46 +0300

    add linter

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const random = require('./lib/random')
 
@@ -7,27 +7,27 @@ const PORT = 8000
 const app = express()
 
 app.engine('handlebars', expressHandlebars({
-    defaultLayout: 'main',
+  defaultLayout: 'main'
 }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => { res.render('home') })
-app.get('/about', (req, res) => { 
-    res.render('about', { rnd: random.getRandom() }) 
+app.get('/about', (req, res) => {
+  res.render('about', { rnd: random.getRandom() })
 })
 
 app.use((req, res) => {
-    res.status(404)
-    res.render('404')
+  res.status(404)
+  res.render('404')
 })
 app.use((err, req, res, next) => {
-    console.log(err.message);
-    res.status(500)
-    res.render('500')
+  console.log(err.message)
+  res.status(500)
+  res.render('500')
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Express запущен ${PORT} на порту`)
-});
+app.listen(PORT, () => {
+  console.log(`Express запущен ${PORT} на порту`)
+})
