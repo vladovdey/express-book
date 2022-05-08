f2fec7601332dcbf10b831ff5828b7fcbde280ac
Vladislav Ovdeychuk <ovdeyvlad@gmail.com>
2022-05-08 14:14:14 +0300

    Chapter 7

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+
 const random = require('./lib/random')
+const weatherMiddlware = require('./lib/middleware/weather')
 
 const PORT = 8000
 
 const app = express()
 
+app.disable('x-powered-by')
 app.engine('handlebars', expressHandlebars({
-  defaultLayout: 'main'
+  defaultLayout: 'main',
+  helpers: {
+    section: function (name, options) {
+      if (!this.sections) this.sections = {}
+      this.sections[name] = options.fn(this)
+      return null
+    }
+  }
 }))
 app.set('view engine', 'handlebars')
 
+/* eslint-disable n/no-path-concat */
 app.use(express.static(__dirname + '/public'))
+/* eslint-enable n/no-path-concat */
+app.use(weatherMiddlware)
 
 app.get('/', (req, res) => { res.render('home') })
-app.get('/about', (req, res) => {
-  res.render('about', { rnd: random.getRandom() })
+app.get('/about', (req, res) => { res.render('about', { rnd: random.getRandom() }) })
+app.get('/test-section', (req, res) => res.render('test-section'))
+app.get('/headers', (req, res) => {
+  res.type('text/html')
+  const headers = Object.entries(req.headers)
+    .map(([key, value]) => `${key} : ${value} \n`)
+  res.send(headers.join(''))
 })
 
 app.use((req, res) => {
